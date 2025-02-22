4d88d8915305f878464197bbe69bb7bff8984288
sacm <sacm1046@gmail.com>
2025-02-22 16:49:33 -0300

    every 5 min

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function miMetodo(param) {
-    console.log(`Ejecutando miMetodo con el parámetro: ${param}`);
+    console.log(`Ejecutando miMetodo con el parámetro: ${param} a las ${new Date()}`);
     // Aquí agregas la lógica que necesites
 }
 
